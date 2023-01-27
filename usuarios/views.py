3f752c574ead0878a64559b4ff0338eb332dc103
valmirdeveloper <valmirdeveloper@gmail.com>
2023-01-27 18:12:19 -0300

    fiz a validação do formulário

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(confirmar_senha.strip()) == 0:
+            return render(request, 'cadastro.html')
+
+        if senha != confirmar_senha:
+            return render(request, 'cadastro.html')
+              
+        return HttpResponse(f'{nome}, {email}, {senha}, {confirmar_senha}')
+  
+      
